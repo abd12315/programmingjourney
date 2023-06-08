85bba7f7c1dd9f748f9ad14f7d5471e9b1d576a9
abd12315 <67475230+abd12315@users.noreply.github.com>
2023-06-08 17:00:33 +0500

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,15 +1,16 @@
 a=input("Enter your name :")
 b=input("Enter your 2nd name :")
-print(str(a)+" jumps over the "+str(b))
-print(str(b)+" kick the dog's during sleep "+str(a)+" is a pig ")
+print(f"{a} quick brown fox jumps over {b}")
+print(f"{b} kick the dog's during sleep {a} is a pig ")
 # this is a comment for testing purpose asap
 c=input("Enter First value: ")
 d=input("Enter the second value ")
-z=int(str(c))*int(str(d))
+z=int(c)*int(d)
 print("potara fusion value "+str(int(z)))
 e=input("Enter any valid number ranging from 3.0 and 5.0 : ")
+g=2*float(e)*2*3.14
 if float(e)<=5.0 and float(e)>=3.0:
-        print(str(float(2*(str(e))*2*3.14))+" Percent ratio value among the masses")
+        print(f"{g} Percent ratio value among the masses")
         
 else:
         print("Invalid input value range please consult the documentation for furthur instruction ")
