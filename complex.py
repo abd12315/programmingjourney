80423a92c9bddda0585db7e424b403e9d8070563
abd12315 <67475230+abd12315@users.noreply.github.com>
2023-06-08 17:18:26 +0500

    Create complex.py

---
+++ complex.py
@@ -0,0 +1,7 @@
+a=input("Please input any number :")
+b=input(f"{a} this is the number would you proceed with the multiplication:YorN ")
+if b=='Y' or b=='y':
+    print(int(a)*complex(4.53e-7j))
+else:
+    print("thank you for running the program please leave the suggestion down below to add some changes ... ")
+
