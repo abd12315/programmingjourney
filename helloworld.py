ee09b2c2e6c6b4e40c8c4c63cfa79b5de360e2d0
abd12315 <67475230+abd12315@users.noreply.github.com>
2023-06-08 15:53:17 +0500

    MyFirstProgram
    The first ever program created by me with significant errors

---
+++ helloworld.py
@@ -0,0 +1,19 @@
+a=input("Enter your name :")
+b=input("Enter your 2nd name :")
+print(str(a)+" jumps over the "+str(b))
+print(str(b)+" kick the dog's during sleep "+str(a)+" is a pig ")
+# this is a comment for testing purpose asap
+c=input("Enter First value: ")
+d=input("Enter the second value ")
+z=int(str(c))*int(str(d))
+print("potara fusion value "+str(int(z)))
+e=input("Enter any valid number ranging from 3.0 and 5.0 : ")
+if float(e)<=5.0 and float(e)>=3.0:
+        print(str(float(f=2*(str(e))*2*3.14))+" Percent ratio value among the masses")
+        
+else:
+        print("Invalid input value range please consult the documentation for furthur instruction ")
+print("\t"+str(int(f)))
+
+#code inserted is wrong first ever code
+#rewriting of code in next module
