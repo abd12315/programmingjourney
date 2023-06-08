3035b7b199aab9f54369f09eb4529cc1fddcff5c
abd12315 <67475230+abd12315@users.noreply.github.com>
2023-06-08 16:35:01 +0500

    helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -9,11 +9,10 @@ z=int(str(c))*int(str(d))
 print("potara fusion value "+str(int(z)))
 e=input("Enter any valid number ranging from 3.0 and 5.0 : ")
 if float(e)<=5.0 and float(e)>=3.0:
-        print(str(float(f=2*(str(e))*2*3.14))+" Percent ratio value among the masses")
+        print(str(float(2*(str(e))*2*3.14))+" Percent ratio value among the masses")
         
 else:
         print("Invalid input value range please consult the documentation for furthur instruction ")
-print("\t"+str(int(f)))
 
 #code inserted is wrong first ever code
 #rewriting of code in next module
