47bdab5b97b0855b8e9ed97469153213da041555
abd12315 <67475230+abd12315@users.noreply.github.com>
2023-06-08 22:49:42 +0500

    Create gameprogramvialist.py

---
+++ gameprogramvialist.py
@@ -0,0 +1,30 @@
+players=['Ahmed','Waqar','Rehman']
+weapons=['hammer','sheild','long sword']
+attack_points=[23,34,47]
+print("Welcome to my game .... ")
+char=input("The rules are simple first choose your character i.e Ahmed,Waqar,Rehman: ")
+if char==players[0]:
+    print(f"congraculations you have chosen {players[0]} Your weapon is {weapons[0]}")
+if attack_points
+for weapons[0]:
+print(f"your attack point is {attack_points[0]}")
+if attack_points[0]>attack_points[1] and attack_points[0]>attack_points[2]:
+    print(f"{players[0]} wins the game")
+else:
+    print("Sorry you lose!")
+if char==players[1]:
+    print(f"congraculations you have chosen {players[1]} your weapon is {weapons[1]}")
+for weapons[1]:
+print(f"your attak point is {attack_points[1]}")
+if attack_points[1]>attack_points[0] and attack_points[1]>attack_points[2]:
+   print(f"{players[1]} is the winner ")
+else:
+    print("sorry you lose! ") 
+if char==players[2]:
+    print(f"congraculations you have chosen {players[2]} your weapon is {weapons[2]}")
+
+if attack_points[2]>attack_points[0] and attack_points[2]>attack_points[1]:
+   
+
+
+#do not write code under maintainece
