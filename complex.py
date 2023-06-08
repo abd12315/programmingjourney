6e8cb4da6991beaca0946bf0ab382ccf06543c18
abd12315 <67475230+abd12315@users.noreply.github.com>
2023-06-08 17:19:44 +0500

    Update complex.py

--- complex.py
+++ complex.py
@@ -1,5 +1,7 @@
 a=input("Please input any number :")
 b=input(f"{a} this is the number would you proceed with the multiplication:YorN ")
+# please select a single char value with Y or N value
+# Y representing yes or N representing as NO
 if b=='Y' or b=='y':
     print(int(a)*complex(4.53e-7j))
 else:
