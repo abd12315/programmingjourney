d6b720f3ca4e9dd6f9ca16a9996e59a01fa437fe
abd12315 <67475230+abd12315@users.noreply.github.com>
2023-06-08 17:31:02 +0500

    Update long.py

--- long.py
+++ long.py
@@ -1,2 +1,4 @@
 a=input("input a random long variable *remember to keep the value within the range: ")
+# surprisingly python 3 supports int data type by deafult
 print(a)
+#so that's the physical demonstration of long data type
