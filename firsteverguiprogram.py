1e3e9552224f5ac92879ac17a22f21f6b25b8d07
abd12315 <67475230+abd12315@users.noreply.github.com>
2023-06-10 12:44:04 +0500

    Update firsteverguiprogram.py

--- firsteverguiprogram.py
+++ firsteverguiprogram.py
@@ -1,3 +1,17 @@
 import tkinter
+from tkinter import messagebox
+import tkinter.font
 top=tkinter.Tk()
+def helloCallBack():
+    messagebox.showinfo( "Look who's shown himself here you piece of shit")
+def buttonclicked():
+    print("I warned you")
+C=tkinter.Button(text='Click Here',command=buttonclicked)
+B = tkinter.Button(top,background="grey", text ="Don't Click here", command = helloCallBack)   
+D = tkinter.Canvas(top, bg="blue", height=250, width=300)
+coord = 10, 50, 240, 210
+arc = D.create_arc(coord, start=0, extent=150, fill="red")
+B.pack()
+C.pack()
+D.pack()
 top.mainloop()
