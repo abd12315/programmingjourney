54e2360db2079ee442f206f1691853161e07d8d9
abd12315 <67475230+abd12315@users.noreply.github.com>
2023-06-10 11:34:13 +0500

    Update gameprogramvialist.py

--- gameprogramvialist.py
+++ gameprogramvialist.py
@@ -5,25 +5,19 @@ print("Welcome to my game .... ")
 char=input("The rules are simple first choose your character i.e Ahmed,Waqar,Rehman: ")
 if char==players[0]:
     print(f"congraculations you have chosen {players[0]} Your weapon is {weapons[0]}")
-if attack_points
-for weapons[0]:
-print(f"your attack point is {attack_points[0]}")
 if attack_points[0]>attack_points[1] and attack_points[0]>attack_points[2]:
-    print(f"{players[0]} wins the game")
-else:
-    print("Sorry you lose!")
-if char==players[1]:
-    print(f"congraculations you have chosen {players[1]} your weapon is {weapons[1]}")
-for weapons[1]:
-print(f"your attak point is {attack_points[1]}")
+    print(f"{players[0]} is the winner ")
+elif char==players[1]:
+    print(f"congraculations you have been chosen {players[1]} as your combatant with weapon {weapons[1]}")
 if attack_points[1]>attack_points[0] and attack_points[1]>attack_points[2]:
-   print(f"{players[1]} is the winner ")
+    print(f"{players[1]} is the winner " )
+elif char==players[2]:
+    print(f"congraculation you've selected {players[2]} is as your character with {weapons[2]}")
+if attack_points[2]>attack_points[1] and attack_points[2]>attack_points[0]:
+    print(f"congraculatiion {players[2]} you've won")
 else:
-    print("sorry you lose! ") 
-if char==players[2]:
-    print(f"congraculations you have chosen {players[2]} your weapon is {weapons[2]}")
-
-if attack_points[2]>attack_points[0] and attack_points[2]>attack_points[1]:
+    print("Please rewrit your choices again and contac the developer for furthur questions ")
+    
    
 
 
