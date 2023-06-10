dc98e64252db9ca889d5b5cd95021aec4bfea554
abd12315 <67475230+abd12315@users.noreply.github.com>
2023-06-10 13:03:21 +0500

    Create Myfirstguiprogram.py

---
+++ gui based program/Myfirstguiprogram.py
@@ -0,0 +1,8 @@
+from tkinter import* 
+root=Tk()
+A=Label(root, text="hello there!")
+B=Label(root, text="This is the library following the tkinter which supports gui based operations")
+A.pack()
+B.pack()
+root.mainloop()
+
